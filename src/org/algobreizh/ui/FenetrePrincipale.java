ae3a8c0a11a7413e9e624c119527cf7ac56f174d
Konosprod <konosprod@free.fr>
2016-02-16 15:21:40 +0100

    Ajout de la fenetre principale
    Signed-off-by: Konosprod <konosprod@free.fr>

---
+++ src/org/algobreizh/ui/FenetrePrincipale.java
@@ -0,0 +1,13 @@
+package org.algobreizh.ui;
+
+import javax.swing.JFrame;
+
+public class FenetrePrincipale extends JFrame {
+	
+	static final long serialVersionUID = -2620432862549001611L;
+
+	public FenetrePrincipale() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
