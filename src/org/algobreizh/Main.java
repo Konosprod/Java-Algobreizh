8f72f8bbd5c8cc53c80835d2b8a099b9dadf4002
Konosprod <konosprod@free.fr>
2016-02-22 14:32:47 +0100

    reset Main, fu Loic.
    Signed-off-by: Konosprod <konosprod@free.fr>

--- src/org/algobreizh/Main.java
+++ src/org/algobreizh/Main.java
@@ -1,13 +1,9 @@
 package org.algobreizh;
 
-import org.algobreizh.ui.FenetreRdv;
-
 public class Main {
 
 	public static void main(String[] args) {
-		
-		FenetreRdv coucou = new FenetreRdv();
-		
+
 	}
 	
 }
--- src/org/algobreizh/ui/FenetrePrincipale.java
+++ src/org/algobreizh/ui/FenetrePrincipale.java
@@ -1,13 +1,57 @@
 package org.algobreizh.ui;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTable;
+import javax.swing.SpringLayout;
 
 public class FenetrePrincipale extends JFrame {
 	
 	static final long serialVersionUID = -2620432862549001611L;
 
+	private JPanel mainPane;
+	private SpringLayout layout;
+	private JLabel nomCommercial;
+	private JLabel emailCommercial;
+	private JLabel prenomCommercial;
+	private JLabel numeroCommercial;
+	private JTable tabClient;
+	
 	public FenetrePrincipale() {
-		// TODO Auto-generated constructor stub
+		
+		mainPane = new JPanel();
+		layout = new SpringLayout();
+		nomCommercial = new JLabel("Rambo");
+		emailCommercial = new JLabel("john.rambo@algobreizh.fr");
+		prenomCommercial = new JLabel("John");
+		numeroCommercial = new JLabel("0649784545");
+		tabClient = new JTable();
+		
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		//Nom Prenom commercial
+		mainPane.add(nomCommercial);
+		mainPane.add(prenomCommercial);
+		layout.putConstraint(SpringLayout.WEST, nomCommercial, 10, SpringLayout.WEST, mainPane);
+		layout.putConstraint(SpringLayout.NORTH, mainPane, 10, SpringLayout.NORTH, prenomCommercial);
+		layout.putConstraint(SpringLayout.NORTH, mainPane, 10, SpringLayout.NORTH, nomCommercial);
+		layout.putConstraint(SpringLayout.WEST, prenomCommercial, 10, SpringLayout.EAST, nomCommercial);
+		
+		//Numero et email
+		mainPane.add(emailCommercial);
+		mainPane.add(numeroCommercial);
+		layout.putConstraint(SpringLayout.WEST, emailCommercial, 10, SpringLayout.WEST, mainPane);
+		layout.putConstraint(SpringLayout.NORTH, emailCommercial, 20, SpringLayout.SOUTH, prenomCommercial);
+		layout.putConstraint(SpringLayout.NORTH, numeroCommercial, 20, SpringLayout.SOUTH, prenomCommercial);
+		layout.putConstraint(SpringLayout.WEST, numeroCommercial, 10, SpringLayout.EAST, emailCommercial);
+		
+		
+		mainPane.setLayout(layout);
+		
+		setContentPane(mainPane);
+		setSize(800, 600);
+		setResizable(false);
 	}
 
 }
