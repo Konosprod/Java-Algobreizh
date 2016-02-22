5904da8601eec0b7b931311a2723d6107af8dfbe
Konosprod <konosprod@free.fr>
2016-02-22 16:08:33 +0100

    Design FenetrePrincipale done
    Signed-off-by: Konosprod <konosprod@free.fr>

--- src/org/algobreizh/ui/FenetrePrincipale.java
+++ src/org/algobreizh/ui/FenetrePrincipale.java
@@ -1,8 +1,13 @@
 package org.algobreizh.ui;
 
+import java.awt.BorderLayout;
+
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.SpringLayout;
 
@@ -17,6 +22,10 @@ public class FenetrePrincipale extends JFrame {
 	private JLabel prenomCommercial;
 	private JLabel numeroCommercial;
 	private JTable tabClient;
+	private JButton buttonEdit;
+	private JButton buttonRdv;
+	private JButton buttonAjout;
+	private JButton buttonSuppr;
 	
 	public FenetrePrincipale() {
 		
@@ -27,6 +36,11 @@ public class FenetrePrincipale extends JFrame {
 		prenomCommercial = new JLabel("John");
 		numeroCommercial = new JLabel("0649784545");
 		tabClient = new JTable();
+		buttonEdit = new JButton("Editer");
+		buttonRdv = new JButton("RDV");
+		buttonAjout = new JButton("Ajout");
+		buttonSuppr = new JButton("Suppr");
+		
 		
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
@@ -46,6 +60,40 @@ public class FenetrePrincipale extends JFrame {
 		layout.putConstraint(SpringLayout.NORTH, numeroCommercial, 20, SpringLayout.SOUTH, prenomCommercial);
 		layout.putConstraint(SpringLayout.WEST, numeroCommercial, 10, SpringLayout.EAST, emailCommercial);
 		
+		//Tableau client
+		tabClient = new JTable(new String[][]{{"Test", "test", "test"},{"Test1", "test2", "test3"}}, new String[] {"H1", "H2", "H3"});
+		tabClient.setFillsViewportHeight(true);
+		JScrollPane scrollPan = new JScrollPane(tabClient);
+		mainPane.add(scrollPan);
+		
+		layout.putConstraint(SpringLayout.WEST, scrollPan, 10, SpringLayout.WEST, mainPane);
+		layout.putConstraint(SpringLayout.NORTH, scrollPan, 20, SpringLayout.SOUTH, emailCommercial);
+		layout.putConstraint(SpringLayout.SOUTH, scrollPan, -10, SpringLayout.SOUTH, mainPane);
+		layout.putConstraint(SpringLayout.EAST, scrollPan, -100, SpringLayout.EAST, mainPane);
+		
+		//Boutton Rendez-vous
+		mainPane.add(buttonRdv);
+		layout.putConstraint(SpringLayout.EAST, buttonRdv, -10, SpringLayout.EAST, mainPane);
+		layout.putConstraint(SpringLayout.WEST, buttonRdv, 10, SpringLayout.EAST, scrollPan);
+		layout.putConstraint(SpringLayout.NORTH, buttonRdv, 20, SpringLayout.SOUTH, emailCommercial);
+		
+		//Boutton ajout
+		mainPane.add(buttonAjout);
+		layout.putConstraint(SpringLayout.EAST, buttonAjout, -10, SpringLayout.EAST, mainPane);
+		layout.putConstraint(SpringLayout.WEST, buttonAjout, 10, SpringLayout.EAST, scrollPan);
+		layout.putConstraint(SpringLayout.NORTH, buttonAjout, 10, SpringLayout.SOUTH, buttonRdv);
+		
+		//Button edit
+		mainPane.add(buttonEdit);
+		layout.putConstraint(SpringLayout.EAST, buttonEdit, -10, SpringLayout.EAST, mainPane);
+		layout.putConstraint(SpringLayout.WEST, buttonEdit, 10, SpringLayout.EAST, scrollPan);
+		layout.putConstraint(SpringLayout.NORTH, buttonEdit, 10, SpringLayout.SOUTH, buttonAjout);
+		
+		//Button suppr
+		mainPane.add(buttonSuppr);
+		layout.putConstraint(SpringLayout.EAST, buttonSuppr, -10, SpringLayout.EAST, mainPane);
+		layout.putConstraint(SpringLayout.WEST, buttonSuppr, 10, SpringLayout.EAST, scrollPan);
+		layout.putConstraint(SpringLayout.NORTH, buttonSuppr, 10, SpringLayout.SOUTH, buttonEdit);
 		
 		mainPane.setLayout(layout);
 		
