cb191e387716d56cf8d991f200b26b3d3b0ed104
Konosprod <konosprod@free.fr>
2016-02-24 10:35:05 +0100

    Mise à jour du main, n'y touchez plus, JAMAIS.
    Signed-off-by: Konosprod <konosprod@free.fr>

--- src/org/algobreizh/Main.java
+++ src/org/algobreizh/Main.java
@@ -1,11 +1,13 @@
 package org.algobreizh;
 
-import org.algobreizh.ui.FenetreFicheClient;
+import org.algobreizh.ui.FenetreConnexion;
 
 public class Main {
 
 	public static void main(String[] args) {
-
+		FenetreConnexion mainWindow = new FenetreConnexion();
+		
+		mainWindow.setVisible(true);
 	}
 	
 }
